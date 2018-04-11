322721445bfd834fafc596297a5b480f74d6f110
Amanda Ong <ayo3@cornell.edu>
2018-04-10 21:01:08 -0400

    Updated viewbox, created callback func

--- scripts/site.js
+++ scripts/site.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
   // Set constants
-  var width = '100%';
-  var height = '90%';
+  var width = "100%";
+  var height = "90%";
+  var viewWidth = 1050;
+  var viewHeight = 800;
   var padding = 40;
 
-  
+
   var svg = d3.select("body").append("svg")
               .attr("class", "scatterplot")
               .attr("width", width)
               .attr("height", height)
-              .attr("viewBox","0 0 " + " " + width + " " + height)
+              .attr("viewBox","0 0 " + " " + viewWidth + " " + viewHeight)
               .attr("preserveAspectRatio", "xMinYMin meet")
               .style("border", "1px solid #cccccc")
 
-  svg.append("rect")
-    .attr("width", "50px")
-    .attr("height", "50px")
-    .attr("fill", "pink");
+
+
+    d3.csv("condensed_data.csv", callback);
+
+    function callback(error, data) {
+      if (error) throw error;
+
+      // Compute the series names ("y1", "y2", etc.) from the loaded CSV.
+      console.log("hi");
+      var seriesNames = d3.keys(data[0]);
+      console.log(seriesNames);
+    }
 
 
 }); // End DOMContentLoaded
